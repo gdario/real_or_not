f4ea45552134f181250a139e2958bd9e7f5a8289
Giovanni d'Ario <giovanni.dario@roche.com>
2020-08-21 22:16:05 +0200

    Add EDA

--- src/test.py
+++ src/test.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn import feature_extraction, linear_model, model_selection
-from sklearn.model_selection import StratifiedKFold
-
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import RidgeClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 BASE_DIR = Path('/Users/dariog/Learning/Kaggle/real_or_not')
 INPUT_DIR = BASE_DIR/'data/input'
@@ -12,17 +13,16 @@ OUTPUT_DIR = BASE_DIR/'data/output'
 train_df = pd.read_csv(INPUT_DIR/'train.csv')
 test_df = pd.read_csv(INPUT_DIR/'test.csv')
 
-# Preprocessing
-count_vectorizer = feature_extraction.text.CountVectorizer()
-train_vectors = count_vectorizer.fit_transform(train_df["text"])
-test_vectors = count_vectorizer.transform(test_df["text"])
+pipe = Pipeline([
+    ('cvec', CountVectorizer()),
+    ('ridge', RidgeClassifier())
+])
 
-# Classifier
-clf = linear_model.RidgeClassifier()
-scores = model_selection.cross_val_score(
-    clf, train_vectors, train_df["target"],
-    cv=3, scoring="f1")
-clf.fit(train_vectors, train_df["target"])
+# classifier
+scores = cross_val_score(pipe, train_df['text'], train_df["target"],
+                         cv=3, scoring="f1")
+
+kfold = StratifiedKFold()
 
 # We reuse the sample submission replacing our own predictions
 sample_submission = pd.read_csv(INPUT_DIR/'sample_submission.csv')
