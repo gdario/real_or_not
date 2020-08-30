dca88e70b0307d0069c3e9378d1379a6861b46e9
Giovanni d'Ario <giovanni.dario@gmail.com>
2020-08-30 11:51:12 +0200

    Update utils.py

--- nbs/utils.py
+++ nbs/utils.py
@@ -1,5 +1,4 @@
 import re
-import pandas as pd
 from gensim.parsing.preprocessing import strip_non_alphanum
 from gensim.parsing.preprocessing import strip_multiple_whitespaces
 
@@ -14,27 +13,28 @@ def process_text(txt):
     return txt.lower()
 
 
-def clean_df(df, keep_unique=True, paste_keyword=True):
+def add_keyword_target(df):
     df['keyword2'] = df.keyword.fillna('missing')
-    if 'target' in df.columns:
-        df['str_target'] = df.target.apply(lambda x: str(x))
-        df['keyword_target'] = df.keyword2.str.cat(df.str_target)
+    df['str_target'] = df.target.apply(lambda x: str(x))
+    df['keyword_target'] = df.keyword2.str.cat(df.str_target)
+    return df.drop('keyword2', axis=1)
+
+
+def paste_keyword(df):
+    df['keyword3'] = df.keyword.fillna('').str.replace('%20', ' ')
+    df['text'] = df.keyword3.str.cat(df.text, sep=' ')
+    return df.drop('keyword3', axis=1)
+
+
+def clean_df(df, keep_unique=True, paste_kw=True, add_kw_tgt=True):
+    if add_kw_tgt:
+        df = add_keyword_target(df)
+    if paste_kw:
+        df = paste_keyword(df)
     df['clean_text'] = df.text.apply(process_text)
     if keep_unique:
-        df = df.drop(['id', 'location', 'str_target', 'text', 'keyword2'],
-                     axis=1)
+        df = df.drop(['id', 'location', 'text'], axis=1)
+        if 'target' in df.columns:
+            df = df.drop(['str_target'], axis=1)
         df = df[~df.duplicated()]
-    if paste_keyword:
-        df['keyword'] = df.keyword.str.replace('%20', ' ')
-        df['clean_text'] = df.keyword.str.cat(df.clean_text, sep=' ')
     return df
-
-
-if __name__ == '__main__':
-    train_df = pd.read_csv('data/input/train.csv')
-    test_df = pd.read_csv('data/input/test.csv')
-    for k, v in {'train': train_df, 'test': test_df}.items():
-        cleaned = clean_df(v)
-        out_name = 'data/output/cleaned_' + k + '_df.csv'
-        print(out_name)
-        cleaned.to_csv(out_name)
